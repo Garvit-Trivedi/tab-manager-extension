29d9b1bc4de43022b93ed4c331dc3dd8bf9901b5
Garvit Trivedi <garvit.trivedi.cg@gmail.com>
2025-01-01 23:00:03 +0530

    Update background.js

--- extension/background.js
+++ extension/background.js
@@ -1,23 +1,23 @@
-let tabTimes = {};  // Stores time spent 
-let activeTabId = null;  // Stores the active tab
+let tabTimes = {}; 
+let activeTabId = null;  
 
-// Function to save tabTimes to chrome.storage
+
 function saveTabTimes() {
   chrome.storage.local.set({ tabTimes: tabTimes });
 }
 
-// Update active tab time whenever tab is switched
+
 chrome.tabs.onActivated.addListener(function(activeInfo) {
   const activeTabId = activeInfo.tabId;
   const currentTime = Date.now();
 
-  // If there was a previously active tab, store time spent on it
+
   if (tabTimes[activeTabId]) {
     const lastActiveTime = Date.now() - tabTimes[activeTabId].startTime;
     tabTimes[activeTabId].timeSpent += lastActiveTime;
   }
 
-  // Update the active tab's start time and timeSpent
+
   tabTimes[activeTabId] = {
     id: activeTabId,
     startTime: currentTime,
@@ -27,7 +27,7 @@ chrome.tabs.onActivated.addListener(function(activeInfo) {
   saveTabTimes();  
 });
 
-// Clean up the tabTimes when a tab is closed
+
 chrome.tabs.onRemoved.addListener(function(tabId) {
   if (tabTimes[tabId]) {
     delete tabTimes[tabId];  
